3374580e873df2ca073fd900552c67f643f9f257
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2025-03-17 17:25:30 -0300

    desenvolvendo menu

--- src/app/pagina/home/home.component.ts
+++ src/app/pagina/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { TituloMenuComponent } from "../../componente/titulo-menu/titulo-menu.component";
 import { CardGrandeComponent } from "../../componente/card-grande/card-grande.component";
 import { CardPequenoComponent } from "../../componente/card-pequeno/card-pequeno.component";
+import { MenuComponent } from "../../componente/menu/menu.component";
 
 @Component({
   selector: 'app-home',
-  imports: [TituloMenuComponent, CardGrandeComponent, CardPequenoComponent],
+  imports: [TituloMenuComponent, CardGrandeComponent, CardPequenoComponent, MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
