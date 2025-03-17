b83e87006bdf1828d439b4581ebbb9983fa19e23
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2025-03-17 16:28:24 -0300

    componente de tituli-menu desenvolvido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TituloMenuComponent } from "./componente/titulo-menu/titulo-menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TituloMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
